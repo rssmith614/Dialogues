0d73d31961e23552b71b91704c3de0a91bc9bde3
Robert <rssmith614@gmail.com>
2023-02-09 17:43:49 -0800

    Added camera via CameraX, basic OCR
    Crashes on my emulator :(

--- app/src/main/java/com/example/dialogues/MainActivity.kt
+++ app/src/main/java/com/example/dialogues/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.dialogues
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import com.example.dialogues.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val isAllPermissionsGranted get() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.name
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    private fun requestPermissions() = ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (isAllPermissionsGranted) {
+                startCamera()
+            } else {
+                Snackbar.make(binding.previewView, "Camera permission not granted. \nCannot perform magic ritual.", Snackbar.LENGTH_LONG).setAction("Retry") {
+                    requestPermissions()
+                }.show()
+                Log.e(TAG, "Camera permission not granted")
+            }
+        }
+    }
+
+    private val cameraAdapter = CameraAdapter {
+        Log.d(TAG, "Text Found: $it")
+    }
+
+    private fun startCamera() = cameraAdapter.startCamera(this, this, binding.previewView.createSurfaceProvider())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if (isAllPermissionsGranted) startCamera() else requestPermissions()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraAdapter.shutdown()
     }
 }
