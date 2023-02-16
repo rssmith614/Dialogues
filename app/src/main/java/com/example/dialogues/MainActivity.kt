0c8d9bfe25353300565efd5ffa30674e98cc5c89
gzarate6 <gzarate6@ucmerced.edu>
2023-02-15 17:52:58 -0800

    Boilerplate Translation Stuff

--- app/src/main/java/com/example/dialogues/MainActivity.kt
+++ app/src/main/java/com/example/dialogues/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.dialogues
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+       Setting to English to Spanish for now since other languages
+       requires a machine learning implementation for later
+
+       Source: https://developers.google.com/ml-kit/language/translation/android
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(TranslateLanguage.ENGLISH)
+            .setTargetLanguage(TranslateLanguage.SPANISH)
+            .build()
+        val englishSpanishTranslator = Translation.getClient(options)
+
+        var conditions = DownloadConditions.Builder()
+            .requireWifi()
+            .build()
+        englishSpanishTranslator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {  }
+            .addOnFailureListener{exception -> }
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        textView.text = englishSpanishTranslator.translate("English").toString()
+
+        englishSpanishTranslator.close()
+
     }
+
 }
