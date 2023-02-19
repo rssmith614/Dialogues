a29733138f1a23c91bd8c550289e75e156d94dc9
jgates3 <112346102+jgates3@users.noreply.github.com>
2023-02-18 20:00:10 -0800

    Added pitch and speed adjustability for TTS!

--- app/src/main/java/com/example/dialogues/MainActivity.kt
+++ app/src/main/java/com/example/dialogues/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.Button
 import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.Toast
 import java.util.*
 
-
+/*TO-DO LIST (Will probably not implement all of these!)
+1. Add text highlighting while the tts is speaking (pain)
+2. Fix up UI
+3. Add more voices (agony)
+4. Pause functionality
+*/
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
     private lateinit var tts: TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +27,10 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
     override fun onInit(status:Int){
         val duration = Toast.LENGTH_SHORT
         val Toasttext = "Error has occurred!"
-        val Toasttext2 = "Language current not available at this moment!"
         val Toasttext3 = "Input cannot be blank"
         var btnbind = findViewById<Button>(R.id.speakbutton)
         var editbtnbind = findViewById<EditText>(R.id.editTex)
+        var stopbind = findViewById<Button>(R.id.stopbutton)
 
         btnbind.setOnClickListener { //listens to when user clicks on button
             if (status == TextToSpeech.ERROR) {
@@ -36,12 +42,39 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
                 if (text == null || text == " " || text ==""){
                     Toast.makeText(getApplicationContext(), Toasttext3, duration).show() //displays error to user
                 }
-                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) { //not tested :)
-                    Toast.makeText(getApplicationContext(), Toasttext2, duration).show()
-                }
+
+                val talkspeed = BarSpeed()
+                val pitchvoice = BarPitch()
+                tts.setSpeechRate(talkspeed)
+                tts.setPitch(pitchvoice)
                 tts.speak(text, TextToSpeech.QUEUE_ADD, null) //tts speaks
+
             }
         }
 
+        stopbind.setOnClickListener {
+            tts.stop()
+        }
+    }
+    fun BarSpeed(): Float {
+        var speedbarz = findViewById<SeekBar>(R.id.speedbar)
+        var talkspeed = speedbarz.progress.toFloat()
+        talkspeed /= 50
+
+        if (talkspeed <0.25){
+            talkspeed = 0.25f
+        }
+        return talkspeed
+    }
+
+    fun BarPitch() :Float {
+        var pitchbarz = findViewById<SeekBar>(R.id.pitchbar)
+        var pitchvoice = pitchbarz.progress.toFloat()
+        pitchvoice /= 50
+
+        if(pitchvoice < 0.1){
+            pitchvoice = 0.1f
+        }
+        return pitchvoice
     }
 }
