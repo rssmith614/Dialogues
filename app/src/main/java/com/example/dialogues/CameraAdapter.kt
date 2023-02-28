ec333246eb3cb5bcb7afe926bb49f9440f18c7c1
Arstal <Arstalkai@gmail.com>
2023-02-27 18:12:43 -0800

    image can be brought up after taking it.

--- app/src/main/java/com/example/dialogues/CameraAdapter.kt
+++ app/src/main/java/com/example/dialogues/CameraAdapter.kt
@@ -1,10 +1,7 @@
 package com.example.dialogues
 
 import android.content.Context
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
@@ -12,6 +9,7 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class CameraAdapter(onTextFound: (String) -> Unit) {
+    private var imageCapture:ImageCapture?=null
     private val imageAnalyzerExecutor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
     private val imageAnalyzer by lazy {
         ImageAnalysis.Builder()
@@ -35,6 +33,9 @@ class CameraAdapter(onTextFound: (String) -> Unit) {
                 unbindAll()
                 bindToLifecycle(lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalyzer)
             }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
         }
         cameraProviderFuture.addListener(runnable, ContextCompat.getMainExecutor(context))
     }
