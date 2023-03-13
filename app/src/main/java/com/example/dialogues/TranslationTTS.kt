ea54fc4f4e7cb1b77debe2bb7d2b8f0d58008cfd
jgates3 <112346102+jgates3@users.noreply.github.com>
2023-03-12 19:26:59 -0700

    Basic Settings Page Layout (NEEDS DRASTIC UI AND VISUAL IMPROVEMENTS)
    Update TranslationTTS.kt
    
    Settings integration
    
    Settings Page with TTS Features + Dark Mode/Light Mode
    
    Translation/TTS & Settings Page Integrated (MOSTLY)
    
    90% integrated missing one feature

--- app/src/main/java/com/example/dialogues/TranslationTTS.kt
+++ app/src/main/java/com/example/dialogues/TranslationTTS.kt
@@ -1,11 +1,17 @@
 package com.example.dialogues
 
 import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.speech.tts.TextToSpeech
+import android.speech.tts.Voice
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
@@ -13,10 +19,28 @@ import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import java.util.*
 
+
+//KNOWN ERRORS/ISSUES:
+//BACK BUTTON FOR SETTINGS WORKS BUT DOESNT REMEMBER THE TEXT ITS SUPPOSE TO SPEAK (STATE ISSUE)
+//PAUSE AFTER EACH WORD FEATURE IS MISSING AND STILL NEEDS TO BE INTEGRATED.
+//IF USER PUTS PROGRESS BAR TO ZERO, IT WILL TALK NORMAL. ADD A RESTRICTION THAT GIVES THE LOWEST PITCH OR SPEED INSTEAD. 
 class TranslationTTS : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private lateinit var tts: TextToSpeech
+    private lateinit var prefs: SharedPreferences
+    private lateinit var prefs2: SharedPreferences
+    private var selectedvoice: String = ""
+    private var pitchvoice: Float = 0.0f
+    private var talkspeed: Float = 0.0f
+    private val barStarter = 50
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translation_tts)
+
+        tts = TextToSpeech(this, this, "com.google.android.tts")
+        prefs = PreferenceManager.getDefaultSharedPreferences(this) // Initialize prefs
+        prefs2 = PreferenceManager.getDefaultSharedPreferences(this) // Initialize prefs
+        talkspeed = prefs.getInt("speed", barStarter).toFloat()/50.0f
+        pitchvoice = prefs2.getInt("pitch", barStarter).toFloat()/50.0f
 
 
         val testString = intent.extras?.getString("Text", "")
@@ -40,8 +64,17 @@ class TranslationTTS : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
 
     override fun onInit(status: Int) {
+        val settingsbind = findViewById<ImageButton>(R.id.settingbutton)
         if (status == TextToSpeech.SUCCESS) {
             Log.d(ContentValues.TAG, "TTS Success")
+        }
+
+
+
+        settingsbind.setOnClickListener {
+            val intent = Intent(this, Settings::class.java)
+            startActivity(intent)
+            //textView.setText("")
         }
     }
 
@@ -67,7 +100,7 @@ class TranslationTTS : AppCompatActivity(), TextToSpeech.OnInitListener {
         englishSpanishTranslator.translate(input)
             .addOnSuccessListener {
                     translatedText -> displayText.text  = translatedText
-                    speakString(tts, translatedText)
+                speakString(tts, translatedText)
 
             }
             .addOnFailureListener { Log.d(ContentValues.TAG, "Translate Failed") }
@@ -76,8 +109,54 @@ class TranslationTTS : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
 
     fun speakString(tts: TextToSpeech, input: String){
+        val talkspeed = prefs.getInt("speed", barStarter ).toFloat()/50.0f
+        val pitchvoice = prefs2.getInt("pitch", barStarter ).toFloat()/50.0f
+        tts.setSpeechRate(talkspeed)
+        tts.setPitch(pitchvoice)
+        val sharedPreferences = getSharedPreferences("VoicePreferences", Context.MODE_PRIVATE)
+        selectedvoice = sharedPreferences.getString("SelectedVoice", "").toString()
+        when (selectedvoice) {
+            "Female 1 (UK)" -> { //BRITISH WOMEN
+                val voice1 = Voice("en-gb-x-gba-network", Locale.US, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
 
-            tts.speak(input, TextToSpeech.QUEUE_ADD, null)
+            }
+            "Female 2 (US)" -> { //American WOMEN
+                val voice1 = Voice("en-us-x-tpf-local", Locale.US, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
+
+            }
+            "Female 3 (IN)" -> { //INDIAN WOMEN
+                val voice1 = Voice("en-in-x-enc-network", Locale.US, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
+
+            }
+            "Female 4 (ES)" -> { //SPANISH MAN
+                val locSpanish = Locale("spa","MEX")
+                val voice1 = Voice("es-es-x-eee-local", locSpanish, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
+            }
+
+            "Male 1 (UK)" -> { //BRITISH MAN
+                val voice1 = Voice("en-gb-x-gbb-local", Locale.US, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
+            }
+            "Male 2 (US)" -> { //AMERICAN MAN
+                val voice1 = Voice("en-us-x-iol-local", Locale.US, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
+            }
+            "Male 3 (IN)" -> { //INDIAN MAN
+                val voice1 = Voice("en-in-x-ene-network", Locale.US, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
+            }
+            "Male 4 (ES)" -> { //SPANISH MAN
+                val locSpanish = Locale("spa","MEX")
+                val voice1 = Voice("es-es-x-eef-local", locSpanish, Voice.QUALITY_NORMAL, Voice.LATENCY_NORMAL, false, null)
+                tts.setVoice(voice1)
+            }
+        }
+
+        tts.speak(input, TextToSpeech.QUEUE_ADD, null)
 
     }
 
